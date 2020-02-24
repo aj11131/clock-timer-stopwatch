6897f86f953794dabfae7e6013fd277a6c792c5f
Austin <ausitn.scott.j@gmail.com>
2020-02-23 18:26:27 -0600

    update modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,11 @@ import { StopwatchComponent } from './stopwatch/stopwatch.component';
 
 
 const routes: Routes = [
+  { path: '', component: ClockComponent },
   { path: 'clock', component: ClockComponent },
   { path: 'timer', component: TimerComponent },
   { path: 'stopwatch', component: StopwatchComponent},
-  { path: '**', component: ClockComponent }
+  { path: '**', redirectTo: 'clock', pathMatch: 'full' }
 ];
 
 @NgModule({
